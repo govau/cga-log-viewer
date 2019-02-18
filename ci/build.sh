6e42e411dd983a85e9e45eb76fc74be976901313
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2019-02-18 14:40:36 +1100

    Update CI

---
+++ ci/build.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+# Tag is not always populated correctly by the docker-image resource (ie it defaults to latest)
+# so use the actual source for tag
+TAG=$(cat src/.git/ref)
+REPO=$(cat img/repository)
+
+cat <<EOF > deployment.yaml
+kind: Service
+apiVersion: v1
+metadata:
+  name: ${ENV}cld-log-viewer
+spec:
+  selector:
+    app: ${ENV}cld-log-viewer
+  ports:
+  - protocol: TCP
+    port: 5601
+    targetPort: 5601
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: ${ENV}cld-cf
+spec:
+  hosts:
+  - "api.system.${ENV}.cld.gov.au"
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+  resolution: DNS
+  location: MESH_EXTERNAL
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: ${ENV}cld-cf
+spec:
+  hosts:
+  - "api.system.${ENV}.cld.gov.au"
+  tls:
+  - match:
+    - port: 443
+      sni_hosts:
+      - "api.system.${ENV}.cld.gov.au"
+    route:
+    - destination:
+        host: "api.system.${ENV}.cld.gov.au"
+        port:
+          number: 443
+      weight: 100
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: ${ENV}cld-uaa
+spec:
+  hosts:
+  - "uaa.system.${ENV}.cld.gov.au"
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+  resolution: DNS
+  location: MESH_EXTERNAL
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: ${ENV}cld-uaa
+spec:
+  hosts:
+  - "uaa.system.${ENV}.cld.gov.au"
+  tls:
+  - match:
+    - port: 443
+      sni_hosts:
+      - "uaa.system.${ENV}.cld.gov.au"
+    route:
+    - destination:
+        host: "uaa.system.${ENV}.cld.gov.au"
+        port:
+          number: 443
+      weight: 100
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${ENV}cld-log-viewer
+  labels:
+    app: ${ENV}cld-log-viewer
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ${ENV}cld-log-viewer
+  template:
+    metadata:
+      labels:
+        app: ${ENV}cld-log-viewer
+    spec:
+      containers:
+      - name: ${ENV}cld-log-viewer
+        image: ${REPO}:${TAG}
+        ports:
+        - name: http
+          containerPort: 5601
+        resources: {limits: {memory: "64Mi", cpu: "100m"}}
+        env:
+        - name: PORT
+          value: "5601"
+        - name: INSECURE
+          value: true # used for cookies, we need to enable TLS serving before re-instating
+        envFrom:
+        - secretRef: {name: ${ENV}cld-log-viewer}
+        - secretRef: {name: shared-log-viewer}
+EOF
+
+cat deployment.yaml
+
+echo $KUBECONFIG > k
+export KUBECONFIG=k
+
+kubectl apply --record -f - < deployment.yaml
+kubectl rollout status deployment.apps/${ENV}cld-log-viewer
---
+++ ci/k8s-bootstrap.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+: ${ES_HOSTNAME?ES_HOSTNAME must be set}
+NAMESPACE=${NAMESPACE:-cf-log-viewer}
+
+ci_user="ci-user"
+
+kubectl apply -f <(cat <<EOF
+# Create our own namespace
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: "${NAMESPACE}"
+---
+# Create a service account for deployment
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: "${ci_user}"
+  namespace: "${NAMESPACE}"
+---
+# Give appropriate permissions for being able to
+# deploy into this namespace
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: ci-user-can-deploy
+  namespace: "${NAMESPACE}"
+roleRef:
+  name: edit
+  kind: ClusterRole
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+- name: "${ci_user}"
+  namespace: "${NAMESPACE}"
+  kind: ServiceAccount
+---
+apiVersion: "authentication.istio.io/v1alpha1"
+kind: "Policy"
+metadata:
+  name: "default"
+spec:
+  peers:
+  - mtls:
+      mode: STRICT
+---
+apiVersion: "networking.istio.io/v1alpha3"
+kind: "DestinationRule"
+metadata:
+  name: "default"
+spec:
+  host: "*.local"
+  trafficPolicy:
+    tls:
+      mode: ISTIO_MUTUAL
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: es
+spec:
+  hosts:
+  - "${ES_HOSTNAME}"
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+  resolution: DNS
+  location: MESH_EXTERNAL
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: es
+spec:
+  hosts:
+  - "${ES_HOSTNAME}"
+  tls:
+  - match:
+    - port: 443
+      sni_hosts:
+      - "${ES_HOSTNAME}"
+    route:
+    - destination:
+        host: "${ES_HOSTNAME}"
+        port:
+          number: 443
+      weight: 100
+EOF
+)
+
+secret="$(kubectl get "serviceaccount/${ci_user}" --namespace "${NAMESPACE}" -o=jsonpath='{.secrets[0].name}')"
+token="$(kubectl get secret "${secret}" --namespace "${NAMESPACE}" -o=jsonpath='{.data.token}' | base64 -d)"
+
+cur_context="$(kubectl config view -o=jsonpath='{.current-context}' --flatten=true)"
+cur_cluster="$(kubectl config view -o=jsonpath="{.contexts[?(@.name==\"${cur_context}\")].context.cluster}" --flatten=true)"
+cur_api_server="$(kubectl config view -o=jsonpath="{.clusters[?(@.name==\"${cur_cluster}\")].cluster.server}" --flatten=true)"
+cur_crt="$(kubectl config view -o=jsonpath="{.clusters[?(@.name==\"${cur_cluster}\")].cluster.certificate-authority-data}" --flatten=true)"
+
+kubeconfig="$(cat <<EOF
+{
+  "apiVersion": "v1",
+  "clusters": [
+    {
+      "cluster": {
+        "certificate-authority-data": "${cur_crt}",
+        "server": "${cur_api_server}"
+      },
+      "name": "kubernetes"
+    }
+  ],
+  "contexts": [
+    {
+      "context": {
+        "cluster": "kubernetes",
+        "user": "${ci_user}",
+        "namespace": "${NAMESPACE}"
+      },
+      "name": "kubernetes"
+    }
+  ],
+  "current-context": "kubernetes",
+  "kind": "Config",
+  "users": [
+    {
+      "name": "${ci_user}",
+      "user": {
+        "token": "${token}"
+      }
+    }
+  ]
+}
+EOF
+)"
+
+echo "You may wish to run the following..."
+echo 'credhub set -n /concourse/apps/log-viewer/kubeconfig -t value -v "$(cat <<EOKUBECONFIG'
+echo "${kubeconfig}"
+echo 'EOKUBECONFIG'
+echo ')"'
+echo
+echo "echo \$KUBECONFIG > k"
+echo "export KUBECONFIG=k"
+echo "kubectl get all"
