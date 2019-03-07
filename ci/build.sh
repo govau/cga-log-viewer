30629dc28e71a3d460a8539c62052dda3b1ce5c5
Adam Eijdenberg <adam.eijdenberg@digital.gov.au>
2019-03-07 15:45:14 +1100

    Fix it all

--- ci/build.sh
+++ ci/build.sh
@@ -7,6 +7,10 @@ set -o pipefail
 # so use the actual source for tag
 TAG=$(cat src/.git/ref)
 REPO=$(cat img/repository)
+LOG_PROXY_TAG=$(cat log-proxy-src/.git/ref)
+LOG_PROXY_REPO=$(cat log-proxy-img/repository)
+ES_PROXY_TAG=$(cat es-proxy-src/.git/ref)
+ES_PROXY_REPO=$(cat es-proxy-img/repository)
 
 cat <<EOF > deployment.yaml
 kind: Service
@@ -113,9 +117,41 @@ spec:
         env:
         - name: PORT
           value: "5601"
+        - name: ES_END_POINT
+          value: http://localhost:9300
         envFrom:
         - secretRef: {name: ${ENV}cld-log-viewer}
         - secretRef: {name: shared-log-viewer}
+      - name: ${ENV}cld-log-proxy
+        image: ${LOG_PROXY_REPO}:${LOG_PROXY_TAG}
+        ports:
+        - name: http
+          containerPort: 9300
+        resources: {limits: {memory: "64Mi", cpu: "100m"}}
+        args:
+        - -listen
+        - :9300
+        - -endpoint
+        - http://localhost:9200
+        - -filter
+        - '{"term":{"@cf.env.keyword":"${ENV}.cld.gov.au"}}'
+        envFrom:
+        - secretRef: {name: aws-es-proxy}
+      - name: ${ENV}cld-es-proxy
+        image: ${ES_PROXY_REPO}:${ES_PROXY_TAG}
+        ports:
+        - name: http
+          containerPort: 9200
+        resources: {limits: {memory: "64Mi", cpu: "100m"}}
+        args:
+        - -listen
+        - :9200
+        - -endpoint
+        - "\$(AWS_ES_URL)"
+        envFrom:
+        - secretRef: {name: aws-es-proxy}
+
+
 ---
 apiVersion: extensions/v1beta1
 kind: Ingress
